3e69a46798bade49e5a1d01351c3642ccb8fe4dd
sujin91kr <sujin91kr@gmail.com>
2025-04-04 13:45:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -558,7 +558,7 @@ def view_data_comb_dashboard():
                 for combo, count in omics_combinations.items()
             ]).sort_values(by = "환자 수", ascending = False)
 
-            st.dataframe(combination_df, use_container_width = True, , hide_index = True)
+            st.dataframe(combination_df, use_container_width = True, hide_index = True)
             
             # 2. 선택한 오믹스 필터링
             valid_omics = sorted(project_df['Omics'].unique())
